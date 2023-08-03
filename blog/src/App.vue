ca5f9ba88eb705fc444567a59d92e3d8e601a5cf
Delyc <d.twizeyima@alustudent.com>
2023-08-03 10:59:22 +0200

    feat: First project

---
+++ blog/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>This is the app page</h1>
+  <TestComp msg="working "/>
+</template>
+
+<script>
+
+import TestComp from './components/TestComp.vue';
+export default {
+  name: 'App',
+  components: {
+    TestComp
+  }
+}
+</script>
---
+++ blog/src/components/TestComp.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Hello, I am a component {{msg}}</h1>
+</template>
+
+<script>
+export default {
+  name: "TestComp",
+  props: {
+    msg: String,
+  },
+};
+</script>
